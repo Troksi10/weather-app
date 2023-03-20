36b64c797d10cf6e03f9d2f883bf80daa1978825
lLija <ilija.trokovski@gmail.com>
2023-03-20 13:15:41 +0100

    weather-app

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApioptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ba0afc5f65msh03615ace523ddb4p188a57jsn5c12aa3a1e17",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
